e74a70e1ad8831f42b4c6ee38079ab86899733d4
Coderzz21 <rdm112005@gmail.com>
2025-11-13 22:00:05 +0530

    Update myApp.js

--- boilerplate-express-main/myApp.js
+++ boilerplate-express-main/myApp.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
 
-// Serve static assets from the /public directory
+// Serve static assets from /public
 app.use('/public', express.static(__dirname + '/public'));
 
 // Serve the HTML file at root path
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
+});
+
+// Serve JSON at /json route
+app.get('/json', function(req, res) {
+  res.json({ "message": "Hello json" });
 });
 
 module.exports = app;
